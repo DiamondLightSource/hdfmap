358d9458882db09b6ee3b5d8ac72bb102bc2692c
Dan Porter <dan.porter@diamond.ac.uk>
2024-06-24 16:29:47 +0100

    initial_commit

--- main.py
+++ main.py
@@ -11,3 +11,12 @@ mymap = hdfmap.create_nexus_map(hdf_file, groups=['measurement'])
 rdr = hdfmap.HdfReloader(hdf_file, hdf_map=mymap)
 
 print(mymap)
+
+[data1, data2] = rdr.get_data(['scan_command', 'start_time'])
+print(data1, data2)
+print(rdr.eval('h[len(h)//2]'))
+print(rdr.format('The energy is {en} keV'))
+
+#all_data = hdfmap.multifile_get_data('dataset_name', [f'file{n}.nxs' for n in range(100)])
+
+
